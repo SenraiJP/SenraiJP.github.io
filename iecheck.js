7b4e64895a8ef7261d9c892c76e6b3a35ae1f00b
goahi <goai@protonmail.com>
2020-07-09 17:39:13 +0900

    index.htmlとindex_ie.htmlを統合

--- iecheck.js
+++ iecheck.js
@@ -1,6 +1,14 @@
 if (navigator.userAgent.indexOf('MSIE') != -1 || navigator.userAgent.indexOf('Trident') != -1) {
-    location.href = "./index_ie.html";
+
 } else {
+    var obj = document.getElementById("noIE");
+    obj.classList.remove("showpanel");
+    obj.classList.add("panel");
+
+    var obj2 = document.getElementById("top");
+    obj2.classList.remove("panel");
+    obj2.classList.add("showpanel");
+
     var el = document.createElement("script");
     el.src = "./script.js";
     document.body.appendChild(el);
