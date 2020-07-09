17fbc5b9a18f87c03ee5b1d6dd83e4220f01ba40
goahi <goai@protonmail.com>
2020-07-09 23:28:15 +0900

    update a page for IE

--- iecheck.js
+++ iecheck.js
@@ -1,10 +1,23 @@
 if (navigator.userAgent.indexOf('MSIE') != -1 || navigator.userAgent.indexOf('Trident') != -1) {
+    var obj = document.getElementById("noIE");
+    obj.classList.remove("panel");
+    obj.classList.add("showpanel");
+
+    var redirectCount = 9;
+    var countdown = function() {
+        console.log(redirectCount);
+        if (redirectCount === 0) {
+            clearInterval();
+            location.href = "https://www.google.com/intl/ja_jp/chrome/";
+        } else {
+            document.getElementById("errorPanelCount").textContent = redirectCount;
+            redirectCount--;
+        }
+    };
+    setInterval(countdown, 1000);
+
 
 } else {
-    var obj = document.getElementById("noIE");
-    obj.classList.remove("showpanel");
-    obj.classList.add("panel");
-
     var obj2 = document.getElementById("top");
     obj2.classList.remove("panel");
     obj2.classList.add("showpanel");
